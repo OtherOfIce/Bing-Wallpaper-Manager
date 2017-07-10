83a08e2c6706653a6e3cd10bc331180e638b8e92
Liam Bradshaw <otherofice@gmail.com>
2017-07-10 13:00:10 +0100

    When viewing an image in the time line, it now properly select the home view.

--- Bing_Wallpaper/Pages/Time Line Page/Time Line Page.xaml.cs
+++ Bing_Wallpaper/Pages/Time Line Page/Time Line Page.xaml.cs
@@ -19,7 +19,7 @@ namespace Bing_Wallpaper.Pages.Time_Line_Page
     public partial class Time_Line_Page : UserControl, ISwitchable
     {
         public readonly BingWallpaperManager manager;
-        
+
 
         public Time_Line_Page(BingWallpaperManager man)
         {
@@ -38,19 +38,21 @@ namespace Bing_Wallpaper.Pages.Time_Line_Page
             MenuItem item = (MenuItem) sender;
             ContextMenu help = (ContextMenu) item.CommandParameter;
             Image image = (Image) help.PlacementTarget;
-            Change_Wallpaper.ChangeWallpaper.SetBackground(Directory.GetCurrentDirectory() + "\\img\\" + System.IO.Path.GetFileName(image.Source.ToString()));
+            Change_Wallpaper.ChangeWallpaper.SetBackground(Directory.GetCurrentDirectory() + "\\img\\" +
+                                                           System.IO.Path.GetFileName(image.Source.ToString()));
         }
 
         private void MenuItem_View_Wallpaper_OnClick(object sender, RoutedEventArgs e)
         {
-            
-            MenuItem item = (MenuItem)sender;
-            ContextMenu help = (ContextMenu)item.CommandParameter;
-            Image image = (Image)help.PlacementTarget;
+
+            MenuItem item = (MenuItem) sender;
+            ContextMenu help = (ContextMenu) item.CommandParameter;
+            Image image = (Image) help.PlacementTarget;
             int imagePos = ThumbnailControl.Items.IndexOf(image.DataContext) + 1;
             manager.ImageNumber = imagePos;
             manager.UpdateWallpaper(manager.ImageList[imagePos]);
             ((PageSwitcher) Application.Current.MainWindow).SwitchHomepage();
+            ((PageSwitcher) Application.Current.MainWindow).HomeButton.IsSelected = true;
         }
     }
 }
